cf708d8346cf2944de9fb6c23a733ba48e54a461
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-30 22:31:11 -0700

    debuggs knight

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -6,4 +6,21 @@ export default class Bishop extends Piece {
       "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" :
       "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"));
   }
+
+  isPossibleMove(src, dest) {
+    return (
+      src - 17 === dest ||
+      src - 10 === dest ||
+      src + 6 === dest ||
+      src + 15 === dest ||
+      src - 15 === dest ||
+      src - 6 === dest ||
+      src + 10 === dest ||
+      src + 17 === dest
+    );
+  }
+
+  getSrcToDestPath() {
+    return [];
+  }
 }
