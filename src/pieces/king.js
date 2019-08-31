d1a5b99100551d3d18eb5cdf3d3615f5d80b1715
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-30 21:26:44 -0700

    codes out king piece

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -1,5 +1,25 @@
 import Piece from './piece';
 
 export default class King extends Piece {
-  
+  constructor(player) {
+    super(player, (player === 1 ? 
+      "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : 
+      "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"));
+  }
+
+  isPossibleMove(src, dest) {
+    return (
+      src - 9 === dest ||
+      src - 8 === dest ||
+      src + 1 === dest ||
+      src + 9 === dest ||
+      src + 8 === dest ||
+      src + 7 === dest ||
+      src - 1 === dest
+    );
+  }
+
+  getSrcToDestPath(src, dest) {
+    return [];
+  }
 }
