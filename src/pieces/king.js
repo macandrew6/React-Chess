7cef23ed89d5d985c8268148d280ad19ed602d7a
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-30 21:17:23 -0700

    creates Piece class as top of inheritance class

---
+++ src/pieces/king.js
@@ -0,0 +1,5 @@
+import Piece from './piece';
+
+export default class King extends Piece {
+  
+}
---
+++ src/pieces/piece.js
@@ -0,0 +1,6 @@
+export default class Piece {
+  constructor(player, iconURL) {
+    this.player = player;
+    this.style = {backgroundImage: `url('${iconURL}')`};
+  }
+}
