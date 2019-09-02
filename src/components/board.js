68de8fd376f0cbbaa13ba18e5f5924fe49598fb1
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-02 13:34:09 -0700

    finishes building board

--- src/components/board.js
+++ src/components/board.js
@@ -20,8 +20,19 @@ export default class Board extends React.Component {
     for (let i = 0; i < 8; i++) {
       const squareRows = [];
       for (let j = 0; j < 8; j++) {
-        
+        const squareShade = (isEven(i) && isEven(j) || (!isEven(i) && !isEven(j) ?
+        "light-square" : "dark-square"));
+        squareRows.push(this.renderSquare((i * 8) + j, squareShade));
       }
+      board.push(<div className="board-row">{squareRows}</div>);
     }
+    
+    return (
+      <div>{board}</div>
+    );
   }
+}
+
+function isEven(n) {
+  return n % 2 === 0;
 }
