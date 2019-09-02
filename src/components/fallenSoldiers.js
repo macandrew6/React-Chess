a51d01ddf6454a07b0f377f072e90b31471bcb3e
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-02 13:11:03 -0700

    adds captured soldier component

---
+++ src/components/fallenSoldiers.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../index.css';
+import Square from './square';
+
+export default class FallenSoldier extends React.Component {
+  renderSquare(square, i, squareShade) {
+    return (
+      <Square
+        piece={square}
+        style={square.style}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">{
+          this.props.whiteFallenSoldiers.map((ws, i) => this.renderSquare(ws, i))
+        }</div>
+        <div className="board-row">{
+          this.props.blackFallenSoldiers.map((bs, i) => this.renderSquare(bs, i))
+        }</div>
+      </div>
+    );
+  }
+}
